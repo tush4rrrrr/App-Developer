bd204540f005d11b29eb05f6437305d6010fbe98
tush4rrrrr <114646263+tush4rrrrr@users.noreply.github.com>
2022-10-08 11:54:08 +0530

    Add files via upload

---
+++ CalciUsingSwitch/ViewController.swift
@@ -0,0 +1,107 @@
+//
+//  ViewController.swift
+//  Ginger
+//
+//  Created by APPLE on 06/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var numberOnScreen:Double = 0
+    var previousNumber:Double = 0
+    var performMathOp = false
+    var operation = 0
+    
+    
+    @IBOutlet weak var calclabel: UILabel!
+    
+    
+   @IBAction func OperatorBtn(_ sender: UIButton)
+    {
+        if calclabel.text != "" && sender.tag != 15 && sender.tag != 16
+        {
+            previousNumber = Double(calclabel.text!)!
+            // Addition
+            if sender.tag == 11
+            {
+                calclabel.text = "+"
+            }
+            // Subtraction
+            else if sender.tag == 12
+            {
+                calclabel.text = "-"
+            }
+            // Division
+            else if sender.tag == 13
+            {
+                calclabel.text = "/"
+            }
+            // Multiplication
+            else if sender.tag == 14
+            {
+                calclabel.text = "*"
+            }
+            operation = sender.tag
+            performMathOp = true
+        }
+        
+        else if sender.tag == 16
+        {
+            switch(operation)
+            {
+            case 11 :
+           calclabel.text = String(previousNumber + numberOnScreen)
+            
+            case 12 :
+                calclabel.text = String(previousNumber - numberOnScreen)
+            case 13 :
+                calclabel.text = String(previousNumber / numberOnScreen)
+            case 14 :
+                calclabel.text = String(previousNumber * numberOnScreen)
+            default : calclabel.text = "Error"
+            }
+        }
+       else if sender.tag == 15
+        {
+        calclabel.text = ""
+        previousNumber = 0
+        numberOnScreen = 0
+        operation = 0
+     }
+       
+    }
+    
+    @IBAction func OperandBtn(_ sender: UIButton)
+    {
+        if performMathOp==true
+        {
+            calclabel.text = String(sender.tag - 1)
+            numberOnScreen = Double(calclabel.text!)!
+            performMathOp = false
+        }
+        else{
+        
+        calclabel.text = calclabel.text! + String(sender.tag - 1)
+        numberOnScreen = Double(calclabel.text!)!
+        
+        }
+        
+    }
+    
+    
+    
+    
+
+    
+    override  func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+
+
